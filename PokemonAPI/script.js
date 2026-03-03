5efd80d7a7708d8b21ef3c5a1999bd40e4b2399d
Lucas Bernardes <lucasbernardes.contato@gmail.com>
2026-03-03 16:10:09 -0300

    adiciona estilo pokeapi

--- PokemonAPI/script.js
+++ PokemonAPI/script.js
@@ -1,23 +1,20 @@
-const inputPoke = document.querySelector('#input-poke');
-const btnPoke = document.querySelector('#btn-poke');
+document.getElementById('btnEnviar').addEventListener('click', () => {
+    const pokemon = document.getElementById('pokemonInput').value.toLowerCase();
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
 
-async function buscarPokemon() {
-    const busca = inputPoke.value.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${busca}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById('pokemonName').innerText = `${data.name} #${data.id}`;
+            
+            const imgUrl = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+            document.getElementById('imageContainer').innerHTML = `<img src="${imgUrl}" width="100px">`;
 
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-
-        document.querySelector('#poke-nome').innerText = `${data.name} #${data.id}`;
-        const imgAnimada = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-        document.querySelector('#poke-img').src = imgAnimada;
-        document.querySelector('#poke-ataque1').innerText = `ATAQUE 1: ${data.moves[0].move.name.toUpperCase()}`;
-        document.querySelector('#poke-ataque2').innerText = `ATAQUE 2: ${data.moves[1].move.name.toUpperCase()}`;
-
-    } catch (error) {
-        alert("Pokémon não encontrado!");
-    }
-}
-
-btnPoke.addEventListener('click', buscarPokemon);
+            document.getElementById('attack1').innerText = `ATAQUE 1: ${data.abilities[0].ability.name}`;
+            document.getElementById('attack2').innerText = `ATAQUE 2: ${data.abilities[1] ? data.abilities[1].ability.name : 'N/A'}`;
+        })
+        .catch(erro => {
+            alert("Pokémon não encontrado!");
+            console.error(erro);
+        });
+});
